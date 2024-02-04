0ce110a8119a43a401a539817f7c0b217a204aff
mik-fedosenko <mik-fedosenko@yandex.ru>
2024-02-04 01:16:49 -0800

    removed node_modules, implemented CORS

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 import axios from 'axios';
 import fs from "fs";
 import https from "https";
+import cors from "cors";
 import usersRoutes from './routes/users.js';
 import * as dotenv from "dotenv";
 dotenv.config();
@@ -18,6 +19,22 @@ const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/auroratime.org/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/auroratime.org/fullchain.pem')
 };
+
+// Dynamic CORS configuration
+const allowedOrigins = ['http://localhost:5173', 'https://auroratime.org'];
+
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../AuroraWeb/client/dist')));
